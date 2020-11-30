75f3d07c3a7fa094160518be8e7f4537cd2129a8
敬宇轩 <676061298@qq.com>
2020-11-30 18:48:26 +0800

    优化溯源信息

--- app/src/main/java/cn/work/suyuan/ui/QualityActivity.kt
+++ app/src/main/java/cn/work/suyuan/ui/QualityActivity.kt
@@ -1,29 +1,29 @@
 package cn.work.suyuan.ui
 
-import android.app.Activity
 import android.content.Context
 import android.content.Intent
-import android.net.Uri
+import android.graphics.Color.red
 import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import cn.work.suyuan.R
 import cn.work.suyuan.common.extensions.toast
 import cn.work.suyuan.common.ui.BaseActivity
-import cn.work.suyuan.event.StringEvent
+import cn.work.suyuan.ui.adapter.QuListAdapter
 import cn.work.suyuan.ui.home.HomeViewModel
 import cn.work.suyuan.util.InjectorUtil
-import cn.work.suyuan.widget.GlideEngine
-import com.huantansheng.easyphotos.EasyPhotos
-import com.huantansheng.easyphotos.models.album.entity.Photo
+import com.yanzhenjie.recyclerview.*
 import kotlinx.android.synthetic.main.layout_import_file.*
+import kotlinx.android.synthetic.main.layout_qutality_list.*
+import kotlinx.android.synthetic.main.layout_tracing_fm.*
 import kotlinx.android.synthetic.main.layout_zhijian.*
-import org.greenrobot.eventbus.EventBus
-import java.io.File
-import java.util.ArrayList
+import kotlinx.android.synthetic.main.layout_zhijian.ivBack
 
 class QualityActivity:BaseActivity() {
+    private val spinnerAdapter = QuListAdapter()
+
 
     private val viewModel by lazy {
         ViewModelProvider(
@@ -34,78 +34,95 @@ class QualityActivity:BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.layout_zhijian)
+        setContentView(R.layout.layout_qutality_list)
+        recyclerQualityList.layoutManager = LinearLayoutManager(this)
+        recyclerQualityList.setSwipeMenuCreator(mSwipeMenuCreator)
+        recyclerQualityList.setOnItemMenuClickListener { menuBridge, adapterPosition ->
+            menuBridge.closeMenu()
+            // 左侧还是右侧菜单：
+            val direction = menuBridge.direction
+            // 菜单在Item中的Position：
+            val menuPosition = menuBridge.position
+            Log.e("菜单1",direction.toString())
+            Log.e("菜单2",menuPosition.toString())
+            Log.e("菜单3",adapterPosition.toString())
+            when (menuPosition) {
+                0 -> {
+                   "点击了修改".toast()
+                }
+                1 -> {
+                    "点击了删除".toast()
+                }
+            }
+        }
+        recyclerQualityList.adapter =spinnerAdapter
         initView()
         initObserver()
+
     }
 
     var zhijianID = ""
     var productFile = ""
     private fun initObserver() {
-        viewModel.sendReportLiveData.observe(this, Observer {
+        viewModel.getQutalityList()
+        viewModel.qutalityListLiveData.observe(this, Observer {
             val rp = it.getOrNull() ?: return@Observer
-            zhijianID = rp.data.toString()
-            if (rp.code == 200) "上传质检报告成功".toast() else rp.msg.toast()
-        })
-        viewModel.upLoadFileLiveData.observe(this, Observer {
-            val rp = it.getOrNull() ?: return@Observer
-            if (rp.code == 200)
-                "上传图片成功".toast()
-                productFile = rp.data.toString()
+            if (rp.code == 200) {
+                Log.e("获取质检报告", rp.data.toString())
+                spinnerAdapter.setList(rp.data)
+//                        "获取质检报告成功".toast()
+//                        zhijianID = id.toString()
+//                        checkQuality.text = reportName
+            }
         })
     }
 
+    override fun onResume() {
+        super.onResume()
+    }
+
     private fun initView() {
+
         ivBack.setOnClickListener {
             finish()
         }
-        ivPic.setOnClickListener {
-            EasyPhotos.createAlbum(activity, true, GlideEngine.getInstance())
-                .setFileProviderAuthority("cn.work.suyuan.fileprovider")
-                .start(103)
+        ivQutality.setOnClickListener {
+            AddQualityActivity.start(this)
         }
-        btnConfirmReport.setOnClickListener {
-            if (editZhijian.text.isEmpty()){
-                "质检单号不能为空".toast()
-                return@setOnClickListener
-            }
-            if (productFile == "") {
-                "请先上传质检图片文件".toast()
-                return@setOnClickListener
-            }
-            viewModel.sendReport(editZhijian.text.toString(),productFile)
+        smartRefresh.setOnRefreshListener{
+            viewModel.getQutalityList()
+            smartRefresh.finishRefresh(1500)
         }
-        tvActionZhijian.setOnClickListener {
-            ScanQrCodeActivity.start(this, object : ScanQrCodeActivity.QrCallBack {
-                override fun qrData(result: String) {
-                    editZhijian.append(result + "\n")
-                }
 
-            })
-        }
+            smartRefresh.setOnLoadMoreListener {
+                smartRefresh.finishLoadMore(2000)
+//                if (currentPage < totalPage) {
+//                    next()
+//                    refreshPack()
+//                } else {
+//                    smartRefresh.finishLoadMore(true)
+//                    "没有更多数据了".toast()
+//                }
+            }
 
     }
 
     companion object{
         fun start(context: Context){
-            context.startActivity(Intent(context,QualityActivity::class.java))
+            context.startActivity(Intent(context, QualityActivity::class.java))
         }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (data == null) return
-        if (resultCode == Activity.RESULT_OK) {
-            if (requestCode == 103) {
-                val resultPhotos: ArrayList<Photo>? =
-                    data.getParcelableArrayListExtra(EasyPhotos.RESULT_PHOTOS)
-                for (f in resultPhotos!!) {
-                    GlideEngine.getInstance()
-                        .loadPhotoNoCircle(this, Uri.parse(f.uri.toString()), ivPic)
-                    val file = File(f.path)
-                    viewModel.upLoadFile(file)
-                }
-            }
+    private val mSwipeMenuCreator =
+        SwipeMenuCreator { leftMenu, rightMenu, position ->
+            val deleteItem = SwipeMenuItem(this@QualityActivity)
+            deleteItem.height = 80
+            deleteItem.setImage(R.mipmap.action_delete)
+            val updateItem = SwipeMenuItem(this@QualityActivity)
+            updateItem.height = 80
+            updateItem.setImage(R.mipmap.action_reset)
+            rightMenu.addMenuItem(updateItem)
+            rightMenu.addMenuItem(deleteItem)
         }
-    }
+
 }
